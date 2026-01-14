bb8b4da0738a950f87ef45934b5d84beafa8c927
Sic-SemperTyrannis <wpolan2741@floridapoly.edu>
2026-01-14 03:51:44 +0000

    Created first python program with print

---
+++ print_test.py
@@ -0,0 +1,5 @@
+my_name = "William Polan"
+print(my_name)
+print("Use four consecutive spaces Per indentation level")
+print("A block is a piece of Python program text that is executed as a unit")
+print("After taking COP2080 I hope to gain the abily to quicking anylis large amounts of data, as well as learn the basic of how AI systems work, lasty I hope to learn new ways of looking and think about how to sove a complex problem")
